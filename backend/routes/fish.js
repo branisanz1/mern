59ff5eb1169ebbbc976e1fb7ab54467299b9a47b
Branis <branis.ainouz@gmail.com>
2020-03-31 16:35:58 +0200

    add authenticationGuard in routes

--- backend/routes/fish.js
+++ backend/routes/fish.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import ensureIsAuthentificated from '../helpers/authentificationGuard';
 import { getAll, add, deleteFish } from '../controllers/fishController';
+import ensureIsAuthentificated from '../helpers/authentificationGuard';
 let fishRouter = express.Router();
 
 fishRouter.get('/', getAll);
