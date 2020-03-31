d823e47fa41c524a98d80dafc16e900fc7dfcafa
Branis <branis.ainouz@gmail.com>
2020-03-31 13:37:02 +0200

    add personController and route person

---
+++ backend/controllers/personController.js
@@ -0,0 +1,38 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jwt-simple';
+import moment from 'moment';
+import Person from '../models/personModel';
+
+export const signUp = async (req, res) => {
+  let person = new Person(req.body);
+  let createPerson = await person.save();
+  res.json(createPerson);
+};
+
+export const login = async (req, res) => {
+  const person = await Person.findOne({ email: req.body.email });
+
+  if (!person) {
+    return res.send('not exist');
+  }
+  const password = req.body.password;
+  bcrypt.compare(password, person.password, function (error, success) {
+    if (success) {
+      res.send('good');
+      const payload = {
+        exp: moment().add(1, 'hour').unix(),
+        iat: moment().unix(),
+        iss: person.id
+      };
+
+      let token = jwt.encode(payload, process.env.TOKEN_SECRET);
+      req.json({
+        firstName: person.firstName,
+        lastName: person.lastName,
+        token: `Bearer ${token}`,
+        expiration: moment().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss')
+      });
+    }
+    res.send('password incorect');
+  });
+};
